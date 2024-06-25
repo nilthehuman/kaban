a8ab07595416b8b46a13aab9fba1f1e1187b16b8
Dániel Arató <nil.the.human@gmail.com>
2024-06-25 12:57:58 +0200

    KabanConfig is aware of its own filepath

--- kaban/config.py
+++ kaban/config.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass, asdict
 
+from pathlib import Path
+
 from tomlkit import comment
 from tomlkit.toml_document import TOMLDocument
 from tomlkit.toml_file import TOMLFile
-
 
 import kaban.defaults as defaults
 
@@ -11,6 +12,7 @@ import kaban.defaults as defaults
 @dataclass
 class KabanConfig:
 
+    path: Path  = defaults.DEFAULT_PATHS.config_file_path
     format: str = 'toml'
     local: bool = False
     quiet: bool = False
@@ -20,22 +22,26 @@ class KabanConfig:
         super().__setattr__(attr, value)
         self.save_to_file()
 
-    def load_from_file(self, filename):
+    def load_from_file(self, filepath=None):
         """Destructively load all config options from the config file."""
         # sic, because KabanControl.__init__ may pass us an explicit None argument
-        if filename is None:
-            filename = defaults.CONFIG_FILE_PATH
-        toml_document = TOMLFile(filename).read()
+        if filepath is not None:
+            self.path = filepath
+        toml_document = TOMLFile(self.path).read()
         for attr_name in toml_document:
             assert hasattr(self, attr_name)
             setattr(self, attr_name, toml_document[attr_name])
 
-    def save_to_file(self, filename=defaults.CONFIG_FILE_PATH):
+    def save_to_file(self, filepath=None):
         """Serialize and write all current settings to file."""
+        if filepath is not None:
+            self.path = filepath
         toml_document = TOMLDocument()
         toml_document.add(comment('kaban config file, edit at your own risk'))
         config_dict = asdict(self)
         for attr_name in config_dict:
+            if 'path' == attr_name:
+                continue
             toml_document.append(attr_name, config_dict[attr_name])
-        TOMLFile(filename).write(toml_document)
+        TOMLFile(self.path).write(toml_document)
 
