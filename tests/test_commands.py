cdbf3990c915a818b6ba9d560886d5dfbf58de4c
Dániel Arató <nil.the.human@gmail.com>
2024-06-25 13:01:00 +0200

    A few halfhearted unit tests

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -3,26 +3,52 @@
 
 from pathlib import Path
 
-from pytest import fixture
+import pytest
 
 from kaban.control import KabanControl
-from kaban.defaults import *
+from kaban.defaults import Paths
 
 
 def test_always_pass():
     assert True
 
 
-@fixture(scope="session")
-def mock_kaban_dir(tmp_path_factory):
-    KABAN_DIR = tmp_path_factory
-
-
-def test_init(mock_kaban_dir):
+@pytest.fixture(scope="session", autouse=True)
+def kaban_control(tmp_path_factory):
     kc = KabanControl()
-    kc.init()
-    assert kc._init_done()
-    assert Path(KABAN_DIR / TOML_FILE_PATH).exists()
+    # mock default paths for the sake of an isolated test environment
+    tmp_path = tmp_path_factory.mktemp(".kaban")
+    kc.paths = Paths( tmp_path
+                    , tmp_path / 'my_kaban_tasks.toml'
+                    , tmp_path / 'my_kaban_tasks.yaml'
+                    , tmp_path / 'config.toml'
+                    )
+    yield kc
+
+
+def test_init(kaban_control):
+    kaban_control.init()
+    assert kaban_control._init_done()
+    assert Path(kaban_control.paths.toml_file_path).exists()
+    assert Path(kaban_control.paths.config_file_path).exists()
+
+
+@pytest.fixture(scope="session")
+def setup_init(kaban_control):
+    kaban_control.init()
+
+
+def test_user(kaban_control, setup_init):
+    kaban_control.args.object = "mreynolds"
+    kaban_control.user()
+    assert True # ?
+
+
+def test_add(setup_init):
+    kaban_control = KabanControl()
+    kaban_control.args.object = "Feed dragon"
+    kaban_control.add()
+    assert True # ?
 
 
 # TODO: more tests to come...
